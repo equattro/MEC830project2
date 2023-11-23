0912dcbc436fc10dc62ff4b447b843f2b8655a58
equattro <151798772+equattro@users.noreply.github.com>
2023-11-22 21:01:03 -0500

    Add files via upload

---
+++ Task2/ApplicationFunctionSet_xxx0.cpp
@@ -0,0 +1,115 @@
+#include "DeviceDriverSet_xxx0.h"
+
+extern DeviceDriverSet_Motor AppMotor;
+
+/*运动方向控制序列*/
+enum SmartRobotCarMotionControl
+{
+  Forward,       //(1)
+  Backward,      //(2)
+  S1,
+  Left,
+  S2,          //(3)
+  Right,         //(4)
+  LeftForward,   //(5)
+  S3,
+  LeftBackward,  //(6)
+  S4,
+  RightForward,  //(7)
+  RightBackward, //(8)
+  stop_it        //(9)
+};               //direction方向:（1）、（2）、 （3）、（4）、（5）、（6）
+
+struct Application_xxx
+{
+  SmartRobotCarMotionControl Motion_Control;
+};
+
+extern Application_xxx Application_SmartRobotCarxxx0;
+
+static void ApplicationFunctionSet_SmartRobotCarMotionControl(SmartRobotCarMotionControl direction, uint8_t is_speed)
+{
+  static uint8_t directionRecord = 0;
+  uint8_t Kp, UpperLimit;
+  uint8_t speed = is_speed;
+  switch (direction)
+  {
+  case /* constant-expression */ Forward:
+    /* code */
+      AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*2,
+                                             /*direction_B*/ direction_just, /*speed_B*/ speed*2, /*controlED*/ control_enable); //Motor control
+    break;
+
+  case /* constant-expression */ Backward:
+    /* code */
+
+      AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*2,
+                                             /*direction_B*/ direction_just, /*speed_B*/ speed*2, /*controlED*/ control_enable); //Motor control
+    break;
+
+  case /* constant-expression */ S1:
+    /* code */
+      AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*0,
+                                             /*direction_B*/ direction_just, /*speed_B*/ speed*0, /*controlED*/ control_enable); //Motor control
+      _delay_ms(250);
+    break;
+
+  case /* constant-expression */ Left:
+   
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_back, /*speed_A*/ speed,
+                                           /*direction_B*/ direction_just, /*speed_B*/ speed*0.55, /*controlED*/ control_enable); //Motor control
+    break;
+
+  case /* constant-expression */ S2:
+    /* code */
+      AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*0,
+                                             /*direction_B*/ direction_just, /*speed_B*/ speed*0, /*controlED*/ control_enable); //Motor control
+      _delay_ms(250);
+    break;
+
+  case /* constant-expression */ Right:
+    
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*2,
+                                           /*direction_B*/ direction_just, /*speed_B*/ speed*2, /*controlED*/ control_enable); //Motor control
+    break;
+  case /* constant-expression */ LeftForward:
+    /* code */
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*2,
+                                           /*direction_B*/ direction_just, /*speed_B*/ speed*2 , /*controlED*/ control_enable); //Motor control
+    break;
+
+  case /* constant-expression */ S3:
+    /* code */
+      AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*0,
+                                             /*direction_B*/ direction_just, /*speed_B*/ speed*0, /*controlED*/ control_enable); //Motor control
+      _delay_ms(250);
+    break;
+
+  case /* constant-expression */ LeftBackward:
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed,
+                                           /*direction_B*/ direction_back, /*speed_B*/ speed*0.55 , /*controlED*/ control_enable); //Motor control
+    break;
+
+  case /* constant-expression */ S4:
+    /* code */
+      AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*0,
+                                             /*direction_B*/ direction_just, /*speed_B*/ speed*0, /*controlED*/ control_enable); //Motor control
+      _delay_ms(250);
+    break;
+
+  case /* constant-expression */ RightForward:
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*2 ,
+                                           /*direction_B*/ direction_just, /*speed_B*/ speed*2, /*controlED*/ control_enable); //Motor control
+    break;
+  case /* constant-expression */ RightBackward:
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_just, /*speed_A*/ speed*2,
+                                           /*direction_B*/ direction_just, /*speed_B*/ speed*2, /*controlED*/ control_enable); //Motor control
+    break;
+  case /* constant-expression */ stop_it:
+    AppMotor.DeviceDriverSet_Motor_control(/*direction_A*/ direction_void, /*speed_A*/ 0,
+                                           /*direction_B*/ direction_void, /*speed_B*/ 0, /*controlED*/ control_enable); //Motor control
+    break;
+  default:
+    break;
+  }
+}
